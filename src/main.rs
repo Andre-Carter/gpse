f3878539c897b560687b1d53176dcf594ce48c9d
Andre Carter <113237622+Andre-Carter@users.noreply.github.com>
2026-09-24 14:23:10 -1000

    cargo check

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use gpse::cli::commands::cli_commands;
 use gpse::date_time::time::physical_time;
+use gpse::mathematical::carter_family::carter_formula_repeat;
+//use gpse::mathematical::dimensional::dimensional::_dimensions;
 use gpse::science::astrological::celestial::{EARTH, MOON};
 use gpse::science::physical::equations::gravitational_force;
 
@@ -7,12 +9,16 @@ fn main() {
     println!("WELCOME TO GPSE!");
     println!("[DATE/TIME]");
 
+    carter_formula_repeat();
+
     println!(
         "{}",
         gravitational_force(EARTH.mass_kg, MOON.mass_kg, 384_400_000.0)
     );
 
     println!("{}", physical_time(EARTH.velocity_m));
+
+
 
     println!("Type \"help\" for commands.");
     cli_commands();
